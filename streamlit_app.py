6da132eaaeabf9a317b6888d6c03922b8791494e
mustafa yalcin <46403193+ada7070@users.noreply.github.com>
2024-08-04 12:40:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ with st.expander('data'):
     st.write('***y***')
     y=data['species']
     y
+    st.write(data.info())
 with st.expander('data visuliation'):
     st.scatter_chart(data=data,x='bill_length_mm',y='body_mass_g',color='species')
     
