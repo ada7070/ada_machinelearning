7efcb24101bff04316ff0c798968bdc36af8226e
mustafa yalcin <46403193+ada7070@users.noreply.github.com>
2024-08-04 11:59:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ st.write('Hello world! mustafa')
 st.write('first try')
 with st.expander('data'):
     data=pd.read_csv('penguins_cleaned.csv')
-    st.write(data)
+    data
     st.write(data.shape)
