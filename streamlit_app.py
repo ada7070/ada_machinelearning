1a60565432fc816e2108eaa229888dba4ccaa21f
mustafa yalcin <46403193+ada7070@users.noreply.github.com>
2024-08-11 19:21:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,6 +115,8 @@ st.dataframe(df_prediction_proba,
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
+st.success('This is a success message!', icon="✅")
+
 
     
       
